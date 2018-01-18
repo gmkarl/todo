5a7dcece887cd11c94c573c6a278ec41a618453c
Karl Semich <fuzzyTew@gmail.com>
2018-01-18 11:52:19 +0000

    exercise stuff

---
+++ exercise/tacfit/2010X860.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+dir="$1"
+PLAYONCE="cvlc --play-and-exit "
+PLAYLOOP="cvlc --loop"
+STOPWATCH="./stopwatch.sh"
+
+echo '=== 1 round of warmup =='
+$PLAYONCE "$dir"/00-warmup/*
+
+echo 'Warmup done, beginning workout in 20'
+$STOPWATCH 20
+
+for ex in "$dir"/01-*
+do
+	echo "=== 8 sets: $(readlink $ex) === "
+	$PLAYLOOP "$ex" &
+	playpid=$!
+	for set in 1 2 3 4 5 6 7 8
+	do
+		echo "=== SET $set ==="
+		echo "Exercise for 20 seconds"
+		$STOPWATCH 20
+		if (( set != 8 ))
+		then
+			kill -stop $playpid
+			echo "Recover for 10 seconds; write your score"
+			$STOPWATCH 10
+			kill -cont $playpid
+		fi
+	done
+	kill $playpid
+
+	echo "== RECOVERY =="
+	echo "Write down your last score, then your heart rate, then technique, effort, and discomfort"
+	$STOPWATCH 60
+done
+
+
+echo '=== 1 round of cooldown ==='
+$PLAYONCE "$dir"/02-cooldown/*
+
+echo '=== DONE ! ==='
+echo 'Write your final heart rate'
+echo 'Sum your lowest set scores'
+echo 'Average your other metrics'
---
+++ exercise/tacfit/stopwatch.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+date1=`date +%s`
+while true
+do 
+	echo -ne "$(date -u --date @$((`date +%s` - date1)) +%H:%M:%S)\r"
+done &
+sleep "$@"
+kill $!
+echo "DONE! $@"
