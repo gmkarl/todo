a8c27ebf870cc40643e9f8c562c388da6136d3a6
Karl Semich <fuzzyTew@gmail.com>
2018-01-23 19:52:06 +0000

    add time stretching

--- exercise/tacfit/mobilitycompensation.sh
+++ exercise/tacfit/mobilitycompensation.sh
@@ -3,16 +3,9 @@
 dir="$1"
 . ./functions
 
-numdrills=$(ls "$dir" | wc -l)
-
 for round in 1 2 3
 do
 	echo "== 6 minutes of drills, round $round / 3 =="
-	if (( numdrills == 6 ))
-	then
-		playall60 "$dir"
-	else
-		playallN "$dir" $((18*60/3/numdrills))
-	fi
+	playalloncefor "$dir" $((18*60/3))
 done
 echo "== done =="
