400f37d6d1e652621bada39270858003885393a8
olpc user <olpc@xo-5d-f7-86.localdomain>
2018-05-27 13:20:22 -0400

    made plans for framing tent in truck

---
+++ Projects/01-Move_to_NRQZ/tent_wood_framing.py
@@ -0,0 +1,141 @@
+#/usr/bin/python
+
+import math
+
+# X is right, Y is front, Z is up
+
+cm_in = 2.54
+mm_cm = 10.0
+mm_m = 1000.0
+in_ft = 12.0
+mm_in = cm_in * mm_cm
+mm_ft = mm_in * in_ft
+
+truck_mm = (7 * mm_ft + 8 * mm_in, 14 * mm_ft + 7./8 * mm_in, 6 * mm_ft + 9 * mm_in)
+tent_mm = (3 * mm_m, 2 * mm_m, 2.3 * mm_m)
+intertent_mm = 50
+
+print "truck", truck_mm, "mm"
+print "tent", tent_mm, "mm"
+
+in_mm = 1.0 / mm_in
+
+# plan: build a frame to fit the tent into the deep end of the box, following orientation
+# above, such that tent is squished with regard to width and height, but leaves much
+# length.
+
+tent_in = [x*in_mm for x in tent_mm]
+truck_in = [round(x*in_mm,10) for x in truck_mm]
+
+# the truck will actually be taller on Z than listed here, because the door is not
+# present in the deep end of the box.
+# the in here are a minimum.
+
+# frame plan: 2x4's sheathed with plywood to fill to <=2"
+# standard lumber lengths are even-numbered 6-24 ft
+# planing is standardized to lose 0.5" from nominal dimension (for 2x's)
+# so 2x4's are 1.5 " deep and we'll want plywood that's a little under
+# a quarter inch to sheathe it.
+
+# plywood likely 4'x8'
+# lumber likely 8' long
+
+# two frames: intertent and inner
+# internet frame goes inbetween tent layers.
+
+intertent_frame_outer_in = (truck_in[0], tent_in[1], truck_in[2]) # note height is minimum
+
+# these dimensions are all smaller than 8 ft (and greater than 6 ft)
+
+# plan for cuts: I'll probably want the floor to be full length so the studs may rest on it
+# I'll want studs in the floor to walk on.  Two ends of the floor will be full size,
+# and perpendicular studs will be decreased size to fill.
+# short studs 'feel' sturdier because the air spaces are smaller, but this means more
+# cuts to make more studs
+# maybe I'll make the smaller airspace approah.
+
+# so, floor length will be full with 2 studs
+# floor width will have successive studs
+# 4 corners, 2 corner studs for each corner
+# ceiling as floor
+# I should cut the inner straps from the tent so I can assemble this.
+# They are no longer needed.
+# studs are typically 16" on center, sometimes dropped to 12" on center
+# I'll do 16" on center studs
+
+class Stud:
+	def __init__(self, dim, length, count, label):
+		self.count = int(math.ceil(count))
+		self.label = label
+		self.dim = (dim[0], dim[1], length)
+	def __str__(self):
+		return "{}x {} long {}x{} studs '{}'".format(self.count, self.dim[2], self.dim[0], self.dim[1], self.label)
+class Panel:
+	def __init__(self, depth, dim, count, label):
+		self.count = count
+		self.label = label
+		self.dim = (dim[0], dim[1], depth)
+	def __str__(self):
+		return "{}x {}x{} {} thick panels '{}'".format(self.count, self.dim[0], self.dim[1], self.dim[2], self.label)
+
+class Frame:
+	def __init__(self, x, y, z, stud_width = 3.5, stud_depth = 1.5, sheathe_depth = 0.25, stud_spacing = 16):
+		self.dim = (x,y,z)
+		self.stud_dim = (stud_width, stud_depth)
+		self.stud_dist = stud_spacing
+		self.sheathe_depth = sheathe_depth
+		if x > y:
+			self.long_dir = 0
+		else:
+			self.long_dir = 1
+		self.studs = []
+		self.panels = []
+
+		# long studs for floor and ceiling, plus 4 for kick and top plates
+		floor_dim = (self.dim[1 - self.long_dir], self.dim[self.long_dir])
+		self.studs.append(Stud(self.stud_dim, floor_dim[1], 4 + 4, 'end stubs & kick and top'))
+
+		# short studs for floor and ceiling
+		inner_len = floor_dim[1] - 2 * self.stud_dim[0]
+		# to calculate how many studs are needed for a wall (including the two
+		#  at the edges) divide the width by the spacing and add 1
+		inner_count = floor_dim[0] / self.stud_dist + 1
+		self.studs.append(Stud(self.stud_dim, inner_len, math.ceil(inner_count) * 2, 'joist'))
+	
+		# kick and top plates for short wall
+		# NOTE kick and top plates need to be sideways to have correct wall depth
+		short_wall_len = floor_dim[0] - self.stud_dim[1]*2 - sheathe_depth*4
+		self.studs.append(Stud(self.stud_dim, short_wall_len, 4, '2 short kick & 2 short top'))
+
+		# tall studs for wall, assume floor and ceiling are sheathed before raising
+		inner_height = self.dim[2]
+		inner_height -= 2 * self.stud_dim[1] - 4 * self.sheathe_depth # floor and ceiling
+		inner_height -= 2 * self.stud_dim[0] # kick and top plate
+		# inner studs, including 2 on each wall making 2-stud corners
+		wall1_stud_count = floor_dim[1] / self.stud_dist + 1
+		wall0_stud_count = short_wall_len / self.stud_dist + 1
+		wall_total_stud_count = (math.ceil(wall0_stud_count) + math.ceil(wall1_stud_count)) * 2
+		self.studs.append(Stud(self.stud_dim, inner_height, wall_total_stud_count, 'wall stud'))
+
+		# the wall outer sheathes do not cover (but are flush with) the kick/top
+		# plates, to ease assembly from the inside
+
+		# floor and ceiling, inner and outer
+		self.panels.append(Panel(self.sheathe_depth, (self.dim[0], self.dim[1]), 4, 'floor & ceiling'))
+
+		# short wall, inner and outer
+		wall_sheathe_height = self.dim[2] - 2 * self.stud_dim[1] - 4 * self.sheathe_depth
+		self.panels.append(Panel(self.sheathe_depth, (short_wall_len, wall_sheathe_height), 4, 'short wall'))
+
+		# long wall, inner and outer
+		self.panels.append(Panel(self.sheathe_depth, (floor_dim[1], wall_sheathe_height), 4, 'long wall'))
+	def __str__(self):
+		ret = ""
+		for stud in self.studs:
+			ret = ret + str(stud) + "\n"
+		for panel in self.panels:
+			ret = ret + str(panel) + "\n"
+		return ret
+
+print str(Frame(intertent_frame_outer_in[0], intertent_frame_outer_in[1], intertent_frame_outer_in[2]))
+print str(Frame(intertent_frame_outer_in[0] - 4, intertent_frame_outer_in[1] - 4, intertent_frame_outer_in[2] - 4))
