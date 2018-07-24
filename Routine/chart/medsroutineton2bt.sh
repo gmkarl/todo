11853ae1cfd3d2ca7d7cbcee77adc33d85d1f87b
Karl Semich <fuzzyTew@gmail.com>
2018-07-24 14:30:54 +0000

    add missing weeks

--- Routine/chart/medsroutineton2bt.sh
+++ Routine/chart/medsroutineton2bt.sh
@@ -22,6 +22,21 @@ done
 } | sort > bt.csv
 rm bt-routine.csv
 
+# make folders for separating data
+for ((yr=2018; yr < $(date +%Y); yr ++))
+do
+	for ((wk=1; wk <=52; wk ++))
+	do
+		mkdir -p ${yr}-$(printf %02d $wk)
+	done
+done
+curwk=$(date +%V)
+curwk=${curwk#0}
+for ((wk=1; wk < curwk; wk ++))
+do
+	mkdir -p $(date +%Y)-$(printf %02d $wk)
+done
+
 # accumulate toothbrushes/wk
 cat bt.csv | while read day
 do
