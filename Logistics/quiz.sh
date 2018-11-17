4e077249513286808783083df413088474984d4e
Karl Semich <fuzzyTew@gmail.com>
2018-11-17 13:26:58 +0000

    2018-11-17 brushing N2 scheduling etc work: 2 min

--- Logistics/quiz.sh
+++ Logistics/quiz.sh
@@ -5,6 +5,7 @@ needed=7
 pat='^I*[0-9]\.* '
 tmp=$(mktemp)
 echo 0  > "$tmp"
+echo 0 >> "$tmp"
 while (( $(<"$tmp") < "$needed" ))
 do
 	grep "$pat"  "$file" | sort -R | head -n 1 |
@@ -12,11 +13,12 @@ do
 		read prefix descr
 		echo "$file '$prefix': what's the rest?"
 		read rest < /dev/tty
+		read correct total < "$tmp"
+		total=$((total + 1))
 		if [ "$rest" = "$descr" ]
 		then
 			echo "Exactly!  Well done!"
-			correct=$(( $(<"$tmp") + 1))
-			echo "$correct" > "$tmp"
+			correct=$((correct + 1))
 		else
 			echo "Not quite right ... it was:"
 			echo "$descr"
@@ -24,6 +26,10 @@ do
 			read < /dev/tty
 			clear
 		fi
+		echo "$correct" > "$tmp"
+		echo "$total" > "$tmp"
 	}
 done
+read correct total < "$tmp"
+echo "$(( correct * 100 / total))% !"
 rm "$tmp"
