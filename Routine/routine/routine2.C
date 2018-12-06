cbd7665514c8257beb4029009f709337beddd5b0
Baffo32 <baffo32@gmail.com>
2018-12-06 11:51:05 +0000

    routine2 stub added

---
+++ Routine/routine/routine2.C
@@ -0,0 +1,36 @@
+/* .C file for routine stuff
+ *
+ * NOW Thu Morn: SCRIPT GOAL judge history.  reorder routine. THEN run routine!  celebrate c++ root
+ *
+ */
+
+
+// arguments? run how?
+// -> script has main function calls script macro function
+// ttpe commands! function calls!
+// make global object? maybe!
+// at end, for crash. (something is wrong with making global object, so put it at the end in case causes problem.  solve later, much later)
+
+// consolide notes
+
+class Routine
+{
+public:
+
+};
+
+Routine gR;
+
+
+void routine2()
+{
+  // macro function
+}
+
+#if 0
+int main(int argc, const char **argv)
+{
+  // haha this will never work soon
+  return 0
+}
+#endif
